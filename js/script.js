7da2bfb00b74760a6acf699e23195347feedc769
mixelio <mixelios@gmail.com>
2021-10-24 20:16:47 +0300

    woadaptiv

--- js/script.js
+++ js/script.js
@@ -1,16 +1,46 @@
 "use strict";
 
-$(document).ready(function() {
-  $(".games__slider").slick({
-    arrows: true,
-    slidesToShow: 3,
-    infinite: true,
-    speed: 1000,
-    dots: false,
-    centerMode: true
-    //autoplay: true
-  });
+new Swiper(".update__swiper", {
+  loop: true,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  slidesPerView: 3,
+  spaceBetween: 70
 });
+
+new Swiper(".games__swiper", {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 90,
+  centeredSlides: true,
+  loopedSlides: 3,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    hideOnClick: !0
+  }
+});
+
+// $(document).ready(function() {
+//   $(".games__slider").slick({
+//     arrows: true,
+//     slidesToShow: 3,
+//     infinite: true,
+//     speed: 1000,
+//     dots: false,
+//     centerMode: true
+//     //autoplay: true
+//   });
+// });
 
 const play = document.querySelector(".play");
 const video = document.querySelector(".content__video");
