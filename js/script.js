bf98b6f9f10945eaf61d79880c29db01272919a6
mixelio <mixelios@gmail.com>
2021-11-04 12:09:03 +0200

    support

--- js/script.js
+++ js/script.js
@@ -90,6 +90,26 @@ if (iconMenu) {
   });
 }
 
+$(function() {
+  // открываем вкладку по умолчанию
+
+  $(".support_faq__spoller_item .spoller__text").slideUp();
+  $(".active__item .spoller__text").slideDown();
+
+  $("#accordion .spoller__title").on("click", function() {
+    $("#accordion .spoller__text")
+      .not($(this).next())
+      .slideUp(299);
+
+    $(".support_faq__spoller_item").removeClass("active__item");
+    $(this)
+      .next()
+      .slideDown(299)
+      .parent()
+      .addClass("active__item");
+  });
+});
+
 const play = document.querySelector(".play");
 const video = document.querySelector(".content__video");
 const playActive = document.querySelector("._playActive");
